cbf9704ba6f8109e398100efea15a50fb902e75b
Giuseppe <giuseppe.canosa@eng.it>
2021-05-19 22:42:14 +0200

    app component with life cycle and test ajax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MessageComponent } from './shared/components/message/message.component';
+import { CommonHttpService } from './shared/services/common-http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit, AfterContentInit, AfterContentChecked {
 
-  constructor(private http: HttpClient) {}
+  @ViewChild(MessageComponent) messageComponent: MessageComponent;
+
+  constructor(
+    private http: CommonHttpService,
+  ) { }
 
   ngOnInit(): void {
-    this.http.get('https://gorest.co.in/public-api/usrs').subscribe();
+    console.log('ON INIT');
+    this.http.get<any>('/users').subscribe();
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+    // console.log(this.messageComponent)
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AFTER CONTENT INIT');
+    // console.log(this.messageComponent)
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AFTER CONTENT CHECKED');
+    // console.log(this.messageComponent)
   }
 
 }

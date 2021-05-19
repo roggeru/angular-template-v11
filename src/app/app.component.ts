09ecc69b49a98f112cb075144d45e9b61fac99cb
Giuseppe <giuseppe.canosa@eng.it>
2021-05-19 22:45:08 +0200

    clear app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MessageComponent } from './shared/components/message/message.component';
 import { CommonHttpService } from './shared/services/common-http.service';
 
 @Component({
@@ -8,8 +7,6 @@ import { CommonHttpService } from './shared/services/common-http.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit, AfterContentInit, AfterContentChecked {
-
-  @ViewChild(MessageComponent) messageComponent: MessageComponent;
 
   constructor(
     private http: CommonHttpService,
@@ -22,17 +19,14 @@ export class AppComponent implements OnInit, AfterViewInit, AfterContentInit, Af
 
   ngAfterViewInit(): void {
     console.log('AFTER VIEW INIT');
-    // console.log(this.messageComponent)
   }
 
   ngAfterContentInit(): void {
     console.log('AFTER CONTENT INIT');
-    // console.log(this.messageComponent)
   }
 
   ngAfterContentChecked(): void {
     console.log('AFTER CONTENT CHECKED');
-    // console.log(this.messageComponent)
   }
 
 }

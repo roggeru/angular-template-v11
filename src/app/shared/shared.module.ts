d7682ff352a14dc83e92a68ca645ebdc49de91f3
Giuseppe <giuseppe.canosa@eng.it>
2021-05-19 22:39:15 +0200

    shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageComponent } from './components/message/message.component';
+import { MessageContainerComponent } from './components/message-container/message-container.component';
+import { AdDirective } from './directives/ad.directive';
+import { AdBannerComponent } from './components/ad-banner/ad-banner.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [MessageComponent, MessageContainerComponent, AdDirective, AdBannerComponent],
+  imports: [CommonModule],
+  exports: [MessageComponent, MessageContainerComponent, AdBannerComponent]
 })
 export class SharedModule { }
